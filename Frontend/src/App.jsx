9e92ebb441e4c12e4c918f3a3513a9ac906cfa27
M Junaid Nasir <jn952060@gmail.com>
2025-01-13 18:18:29 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavBar from './components/NavBar';
-
+import Banner from './components/Banner';
 const App = () => {
-  return (<>
+  return (
+  <>
       <NavBar/>
-      </> )
+      <Banner/>
+      </> 
+      )
 }
 
 export default App;
--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 ">
+          <div className="space-y-6">
+          <h1 className="text-4xl font-bold">
+            Hello,welcomes here to learn something
+            <span className="text-pink-400">new everyday!!!</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam
+            numquam assumenda officiis corporis voluptatum dolorem voluptas illo
+            nam cumque accusamus dolor autem nisi voluptatibus aliquam,
+            doloribus, ad dicta eaque laboriosam.
+          </p>
+        </div>
+        </div>
+        <div className="w-full md:w-1/2 ">Right</div>
+      </div>
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
