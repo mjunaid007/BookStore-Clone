0ed46b778c81ee7cabf1020f600b226a734fb59f
M Junaid Nasir <jn952060@gmail.com>
2025-01-14 01:26:44 +0530

    Cards Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
+import Freebook from './components/Freebook';
+// import Cards from './components/Cards';
 const App = () => {
   return (
   <>
       <NavBar/>
       <Banner/>
+      <Freebook/>
+      {/* <Cards/> */}
       <Footer/>
       </> 
       )
